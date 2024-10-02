1fb5a06b4dc67a9536a1d1463b782b9024620713
codeforcause123 <aayush.130702@gmail.com>
2024-10-02 14:55:04 +0530

    cleanup

--- inventory-service/src/main/java/com/microservice/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventory_service/InventoryServiceApplication.java
@@ -1,11 +1,7 @@
 package com.microservice.inventory_service;
 
-import com.microservice.inventory_service.model.Inventory;
-import com.microservice.inventory_service.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
