fe409c50524a235e08fe5fd1a6b9fb898e2c9eaa
codeforcause123 <aayush.130702@gmail.com>
2024-10-16 18:05:33 +0530

    added circuitbreaker

--- inventory-service/src/main/java/com/microservice/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservice/inventory_service/service/InventoryService.java
@@ -3,18 +3,25 @@ package com.microservice.inventory_service.service;
 import com.microservice.inventory_service.dto.InventoryResponse;
 import com.microservice.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Wait Started");
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
--- order-service/src/main/java/com/mircoservice/order_service/service/OrderService.java
+++ order-service/src/main/java/com/mircoservice/order_service/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -55,6 +55,7 @@ public class OrderService {
 
         if(allProductsInStock) {
             orderRepository.save(order);
+            return "Order Placed Successfully!";
         } else{
             throw new IllegalArgumentException("Product not in stock! Please try again later");
         }
